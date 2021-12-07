20efe230cfc1e27d69fbf31eb02e7ed45ad096b3
Sunil Pottumuttu <spottumuttu@osius.com>
2021-12-07 18:37:34 +0530

    initial version

---
+++ netcoremvcdockervol/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using netcoremvcdockervol.Models;
+using System.Diagnostics;
+using System.Text;
+
+namespace netcoremvcdockervol.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration; 
+
+        public HomeController(ILogger<HomeController> logger,IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            var sb = new StringBuilder();
+            var path = Path.Combine("/usr/share/aspnetcoredockervolume");
+            var files = Directory.GetFiles(path);
+            foreach(var file in files)
+            {
+                sb.Append(file.ToString()+"<br/>");
+            }
+            ViewData["sb"] = sb.ToString();
+            ViewData["sampleconfig"] = _configuration["sampleconfig"].ToString();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ netcoremvcdockervol/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace netcoremvcdockervol.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
